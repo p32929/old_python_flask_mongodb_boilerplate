f7b431ae3f0995b1714dd0e6b87b9747e22236f3
p32929 <p32929@yahoo.com>
2022-02-28 20:51:14 +0600

    added hot reload

--- waitress_server.py
+++ waitress_server.py
@@ -4,11 +4,13 @@ from utils.database_utils import DatabaseUtils
 from utils.routes import app
 import werkzeug.serving
 
+
 @werkzeug.serving.run_with_reloader
 def run_server():
     DatabaseUtils.init_local_db()
     print(f"Listening on port {Constants.PORT}")
     serve(app, host='0.0.0.0', port=Constants.PORT)
+
 
 if __name__ == '__main__':
     run_server()
