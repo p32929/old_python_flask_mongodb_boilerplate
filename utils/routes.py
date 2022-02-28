e2d1d9ae35d33961bd4956f0c70d1bb7ff5ab18d
p32929 <p32929@yahoo.com>
2022-02-28 20:43:32 +0600

    added routes

--- utils/routes.py
+++ utils/routes.py
@@ -1,7 +1,5 @@
 from flask import Flask
-from utils.database_utils import DatabaseUtils
 
-DatabaseUtils.init_local_db()
 app = Flask(__name__)
 app.config["DEBUG"] = False
 app.config["CACHE_TYPE"] = "null"
@@ -9,6 +7,9 @@ app.config["CACHE_TYPE"] = "null"
 
 @app.route("/")
 def hello_world():
-    hello_ver = "Hello World"
+    return "Hello World"
 
-    return hello_ver
+
+@app.route("/users")
+def users():
+    return "Hello users"
--- waitress_server.py
+++ waitress_server.py
@@ -1,6 +1,8 @@
 from waitress import serve
-import server
 from constants import Constants
+from utils.database_utils import DatabaseUtils
+from utils.routes import app
 
+DatabaseUtils.init_local_db()
 print(f"Listening on port {Constants.PORT}")
-serve(server.app, host='0.0.0.0', port=Constants.PORT)
+serve(app, host='0.0.0.0', port=Constants.PORT)
