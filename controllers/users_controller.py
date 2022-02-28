e56dd99de295f5d79a47558c8d68ec9e86062a4f
p32929 <p32929@yahoo.com>
2022-02-28 23:35:17 +0600

    ok get users

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
 
+from services.user_service import UserService
+from utils.importants_defs import Responder
+
 users_controller = Blueprint('users', __name__)
 
 
 @users_controller.route('/')
 def root():
-    return "Hello Users"
+    return Responder.ok(UserService.get_users())
--- utils/importants_defs.py
+++ utils/importants_defs.py
@@ -2,5 +2,16 @@ import json
 from flask import Response
 
 
-def JsonResp(data, status: int):
-    return Response(json.dumps(data, default=str), mimetype="application/json", status=status)
+class Responder:
+
+    @staticmethod
+    def ok(data):
+        return Response(json.dumps(data, default=str), mimetype="application/json", status=200)
+
+    @staticmethod
+    def error_not_found(data):
+        return Response(json.dumps(data, default=str), mimetype="application/json", status=404)
+
+    @staticmethod
+    def error_unauthorized():
+        return Response("UNAUTHORIZED", status=403)
