25349b83ef8538ee8276329fa61cefaedd356493
p32929 <p32929@yahoo.com>
2022-03-02 23:20:40 +0600

    ok so far

--- controllers/root_controller.py
+++ controllers/root_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 root_controller = Blueprint('main', __name__)
 
+
 @root_controller.route('/')
 def root():
     return "Hello World"
--- db_models/session_model.py
+++ db_models/session_model.py
@@ -5,9 +5,6 @@ from db_models.user_model import Users
 
 
 class Sessions(ExtendedDocument):
-    # name = me.StringField()
-    # email = me.StringField()
-    # password = me.StringField()
     user = me.ObjectIdField(Users)
     secret = me.StringField()
 
