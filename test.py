1b873a81d987929ebf1f625208be86a717a24159
p32929 <p32929@yahoo.com>
2022-03-02 10:45:24 +0600

    upgraded deps

--- test.py
+++ test.py
@@ -7,5 +7,6 @@ obj = {
 
 aaa = AuthService.make_access_token(obj)
 aa = AuthService.verify_token(
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IkZGRiIsImZ1bGxOYW1lIjoiRkZGIn0.mh5Y82d1Fav2Pk0dRyZdLoCE8nHBd3WTzwdL6OrelHk')
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IkZGRiIsImZ1bGxOYW1lIjoiRkZGIn0.mh5Y82d1Fav2Pk0dRyZdLoCE8nHBd3WTzwdL6OrelHk'
+)
 print("")
